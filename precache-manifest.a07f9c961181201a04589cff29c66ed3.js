4eedac554cfcdbcb409e0e639fde16bd1d8575c0
Seven-Wave <Seven-Wave@github.com>
2021-01-06 21:52:01 +0300

    Updates

---
+++ precache-manifest.a07f9c961181201a04589cff29c66ed3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e822b33c408719e5d0592bbe80478a05",
+    "url": "/GitClient/index.html"
+  },
+  {
+    "revision": "af98aab9ebea38148ded",
+    "url": "/GitClient/static/css/main.0111782e.chunk.css"
+  },
+  {
+    "revision": "d58445584693b17efa74",
+    "url": "/GitClient/static/js/2.6720e1da.chunk.js"
+  },
+  {
+    "revision": "af98aab9ebea38148ded",
+    "url": "/GitClient/static/js/main.d7203ecc.chunk.js"
+  },
+  {
+    "revision": "b2d18a3bec29d73ab479",
+    "url": "/GitClient/static/js/runtime~main.8f6e3e1a.js"
+  }
+]);
